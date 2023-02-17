352821e1c650b7eebd3d88e4213b266afd7efec1
georgfoe <38016206+georgfoe@users.noreply.github.com>
2023-02-17 23:33:23 +0100

    Update where2meet.r
    Kommentare eingefügt

--- where2meet.r
+++ where2meet.r
@@ -3,7 +3,7 @@ library(dplyr)
 library(jsonlite)
 library(purrr)
 
-
+# Auf Grundlage einer in einem Query-String übergebenen Adresse werden Geo-Daten ermittelt
 query_location <- function(queryString) {
 GET("https://v5.bvg.transport.rest/", 
     path = "locations", 
@@ -12,6 +12,7 @@ GET("https://v5.bvg.transport.rest/",
     pluck(1)
 }
 
+# Alle von einem Ausgangsort (loc) innerhalb von 60 Minuten erreichbaren Stationen werden abgerufen (sortiert nach Fahrtdauer)
 get_reachables <- function(loc) {
   GET("https://v5.bvg.transport.rest/", 
       path = "stops/reachable-from", 
@@ -23,6 +24,7 @@ get_reachables <- function(loc) {
     pluck(content)
 }
 
+# Auf Grundlage der abgerufenen reachables wird ermittelt welche Stationen in duration (in Minuten) oder weniger erreicht werden können
 get_reach_with_duration <- function(reach, duration) {
   reach %>% 
     keep(~.x$duration < duration) %>% 
@@ -32,29 +34,40 @@ get_reach_with_duration <- function(reach, duration) {
     unlist()
 }
 
+# Als Beispiel werden die Geo-Daten der Geschäftsstellen von AWO, Caritas und DRK ermittelt
 loc1 <- query_location("'Blücherstraße 62-63, 10961 Berlin'")
 loc2 <- query_location("'Reinhardtstraße 13, 10117 Berlin'")
 loc3 <- query_location("'Carstennstraße 58, 12205 Berlin'")
 
+# Die von den jeweiligen Locations erreichbaren Stationen werden abgerufen
 reach1 <- get_reachables(loc1)
 reach2 <- get_reachables(loc2)
 reach3 <- get_reachables(loc3)
 
+# Starte die Iteration mit 1 Minute Fahrzeit für alle
 dur = 1
+
+# Initialisiere die Ergebnisvariable hits
 hits = NULL
 
+# Initiiere eine Schleife die wiederholt wird, solange es keine Ergebnisse gibt
 while (length(hits) == 0) {
   
   cat("Teste mit Entfernung ", dur, " min", "\n")
   
+  # Ermittle die in duration von Location 1 erreichbaren Stationen
   hits = get_reach_with_duration(reach1, dur) %>% 
+    # Prüfe welche davon auch von Location 2 in duration erreichbar sind
     keep(~.x %in% get_reach_with_duration(reach2, dur)) %>% 
+    # Prüfe welche davon auch von Location 3 in duration erreichbar sind 
     keep(~.x %in% get_reach_with_duration(reach3, dur))
   
+  # Erhöhe die Fahrtdauer um eine Minute
   dur = dur +1 
   
 }
 
+# Gib das Ergebnis aus
 hits
 
 
